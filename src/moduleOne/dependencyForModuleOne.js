57cd5cb20e28090d24cdff8143f109ee1d4d4acc
Anthony Sennett <anthony.sennett@gmail.com>
2015-02-16 18:03:42 +0100

    adding common module

--- src/moduleOne/dependencyForModuleOne.js
+++ src/moduleOne/dependencyForModuleOne.js
@@ -1,6 +1,7 @@
-define(['underscore'], function (_) {
+define(['underscore', 'common/someCommonModule'], function (_, commonModule) {
 	_.each(['underscore from moduleOne dep'], function(message){
 		console.log(message);
 	});
+	console.log('common bundle from module one dep: ' + commonModule.message);
 	return {};
 });
